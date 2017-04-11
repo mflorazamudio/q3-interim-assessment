d2bdaf241c342baaf0835bf79bf23550d1d9d26f
Maria Flora Zamudio <mflorazamudio@wlapcs.org>
2017-04-11 18:39:32 +0000

    changes

--- sketch.js
+++ sketch.js
@@ -1,29 +1,40 @@
 var x = 150;
 var y =100;
 
+
 function setup(){
   createCanvas(600, 400);
 }
 
 function draw(){
     background(50);
-    bottom();
-    display();
+    
+    for(var x = 50; x < 600; x +=100){
+        bottom(x,300);
+        display(x,300);
+        
+    }
+    
+    if(mouseIsPressed){
+        bottom(mouseX,mouseY);
+        display(mouseX,mouseY);
+    }
+    
     
   
 }
 
-function display(){
+function display(x, y){
    //pop
     noStroke();
-    ellipse(x+100, y+75, 75, 75);
+    ellipse(x, y, 75, 75);
     fill(255);
 
 }
 
-function bottom(){
+function bottom(x, y){
     //the line
-    rect(x+97, y+65, 10, 125);
+    rect(x-5, y-10, 10, 125);
     fill(66, 217, 244);
 }
 
